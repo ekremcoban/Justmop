8212274288b6a24c1abe6b2f4b092bf2cc564c30
Ekrem <ecoban@sutas.com.tr>
2020-08-01 19:15:56 +0300

    vs 0.0.7 --> card  eklendi

--- src/screens/CardsListScreen.js
+++ src/screens/CardsListScreen.js
@@ -1,20 +1,73 @@
-import React from 'react'
-import { Text } from 'react-native';
+import React, { useEffect } from 'react'
+import { Text, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
 import { connect } from 'react-redux';
+import CardFlip from 'react-native-card-flip';
 
 const CardsListScreen = (props) => {
-     console.log(props)
+
+    useEffect(() => {
+
+    }, []);
+
+
+    console.log(props)
+    console.log(props.cards[0].img)
+    const view = (
+        <CardFlip style={styles.cardContainer} ref={(card) => this.card = card} >
+            <TouchableOpacity
+                style={styles.cardFront}
+                onPress={() => this.card.flip()} >
+                <Image resizeMode="contain"
+                    style={styles.tinyLogo}
+                    source={{
+                        uri: props.cards[0].img,
+                    }}></Image>
+                <Text>AB</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.cardBack}
+                onPress={() => this.card.flip()} >
+                <Text>CD</Text>
+            </TouchableOpacity>
+        </CardFlip>
+    )
     return (
         <>
-            <Text>Detay</Text>
+            {props && view}
         </>
     )
 }
 
+const styles = StyleSheet.create({
+    cardContainer: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    cardFront: {
+        width: 400,
+        height: 350,
+        // backgroundColor: 'red',
+    },
+    cardBack: {
+        width: 400,
+        height: 350,
+        marginLeft: "10%",
+        backgroundColor: 'red',
+    },
+    tinyLogo: {
+        width: 400,
+        height: 350,
+        marginTop: 10,
+    },
+});
+
 const mapStateToProps = state => {
     return {
-      cards: state.cards.cards,
+        cards: state.cards.cards,
     };
-  };
+};
 
-  export default connect(mapStateToProps)(CardsListScreen);
+export default connect(mapStateToProps)(CardsListScreen);
